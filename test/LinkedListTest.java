327894ea9a28f0de1165cec0001726fbdfaf3145
aprieger <aprieger@yahoo.com>
2016-04-29 10:23:09 -0700

    Update LinkedListTest.java

--- test/LinkedListTest.java
+++ test/LinkedListTest.java
@@ -45,11 +45,84 @@ public class LinkedListTest {
 		testList.LLInsert(testSubj2);
 		assertEquals("Bob",testList.root.getName());
 	}
-
-	//Not quite sure how to make this one work, perhaps we could use a "toString" into a print function? Might be better.
+	
+	// inserts 3 players into linked list and sets values to them, then attempts to print them out
 	@Test
-	public void testLLPrint() {
-		fail("Not yet implemented");
+	public void testLLInsertAndPrint() {
+		LinkedList playerList = new LinkedList();
+		Player tempInsert1 = new Player();
+		tempInsert1.setName("test1");
+		tempInsert1.setWins(5);
+		tempInsert1.setLosses(4);
+		playerList.LLInsert(tempInsert1);
+		
+		Player tempInsert2 = new Player();
+		tempInsert2.setName("test2");
+		tempInsert2.setWins(3);
+		tempInsert2.setLosses(2);
+		playerList.LLInsert(tempInsert2);
+		
+		Player tempInsert3 = new Player();
+		tempInsert3.setName("test3");
+		tempInsert3.setWins(1);
+		tempInsert3.setLosses(0);
+		playerList.LLInsert(tempInsert3);
+		playerList.LLPrint();
+		
+		assertEquals(playerList.LLPrint(), 
+				"test1: Points: 0, Wins/Losses: 5/4\n"
+				+ "test2: Points: 0, Wins/Losses: 3/2\n"
+				+ "test3: Points: 0, Wins/Losses: 1/0\n");
 	}
-
+	
+	// test the LLSort() method by inserting in random order and sorting based on points
+	@Test
+	public void testLLSort() {
+		LinkedList playerList = new LinkedList();
+		Player tempInsert5 = new Player();
+		tempInsert5.setName("test5");
+		tempInsert5.setPoints(5);
+		tempInsert5.setWins(4);
+		tempInsert5.setLosses(3);
+		playerList.LLInsert(tempInsert5);
+		playerList.LLPrint();
+		
+		Player tempInsert2 = new Player();
+		tempInsert2.setName("test2");
+		tempInsert2.setPoints(2);
+		tempInsert2.setWins(1);
+		tempInsert2.setLosses(0);
+		playerList.LLInsert(tempInsert2);
+		
+		Player tempInsert3 = new Player();
+		tempInsert3.setName("test3");
+		tempInsert3.setPoints(3);
+		tempInsert3.setWins(2);
+		tempInsert3.setLosses(1);
+		playerList.LLInsert(tempInsert3);
+		
+		Player tempInsert1 = new Player();
+		tempInsert1.setName("test1");
+		tempInsert1.setPoints(1);
+		tempInsert1.setWins(0);
+		tempInsert1.setLosses(0);
+		playerList.LLInsert(tempInsert1);
+		playerList.LLPrint();
+		
+		Player tempInsert4 = new Player();
+		tempInsert4.setName("test4");
+		tempInsert4.setPoints(4);
+		tempInsert4.setWins(3);
+		tempInsert4.setLosses(2);
+		playerList.LLInsert(tempInsert4);
+		playerList.LLPrint();
+		
+		playerList.LLSort();
+		assertEquals(playerList.LLPrint(), 
+				"test5: Points: 5, Wins/Losses: 4/3\n"
+						+ "test4: Points: 4, Wins/Losses: 3/2\n"
+						+ "test3: Points: 3, Wins/Losses: 2/1\n"
+						+ "test2: Points: 2, Wins/Losses: 1/0\n"
+						+ "test1: Points: 1, Wins/Losses: 0/0\n");
+	}
 }
