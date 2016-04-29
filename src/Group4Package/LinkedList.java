6354e2ffeeb91894e8ddb75d0c5abe102f60cbd4
aprieger <aprieger@yahoo.com>
2016-04-29 10:19:18 -0700

    Update LinkedList.java

--- src/Group4Package/LinkedList.java
+++ src/Group4Package/LinkedList.java
@@ -1,8 +1,17 @@
 package Group4Package;
 
+/** LinkedList(): Class that handles the Linked List objects and methods
+ * @author Aaron Prieger
+ * @see LLInsert(), LLPrint(), LLSort()
+ */
 public class LinkedList {
 	Player root;
-
+	int nodeCount;
+	
+	/** LLPInsert(): inserts a node into a sorted linked list
+	 * @param insertNode: node to be inserted into linked list
+	 * @return void
+	 */
 	void LLInsert(Player insertNode)
 	{
 		Player tempNode;
@@ -18,15 +27,38 @@ public class LinkedList {
         	insertNode.next = tempNode.next;
         	tempNode.next = insertNode;
         }
+        nodeCount = nodeCount + 1;
 	}
-
-	void LLPrint()
+	
+	/** LLPrint(): prints out the linked list for the leaderboard
+	 * @return void
+	 */
+	String LLPrint()
 	{
 		Player tempNode = root;
-		while (tempNode.next != null)
+		String output="";
+		while (tempNode != null)
 		{
-			System.out.println(tempNode.getName() + ": " + tempNode.getWins() + " wins/" + tempNode.getLosses() + " losses.");
+			output = output + tempNode.getName() + ": Points: " + tempNode.getPoints() + ", Wins/Losses: " + tempNode.getWins() + "/" + tempNode.getLosses() + "\n";
 			tempNode=tempNode.next;
 		}
+		return output;
+	}
+	
+	/** LLSort(): sorts out the linked list for the leaderboard based on point count
+	 */
+	void LLSort()
+	{
+		Player front = root;
+		Player test = root;
+		Player temp;
+		for (int i = 0; i < nodeCount; i++)
+			while (test.next != null)
+			{
+				if (test.getPoints() > front.getPoints())
+					temp = test;
+				test = test.next;
+			}
 	}
 }
+
